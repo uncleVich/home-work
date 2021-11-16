ec8b387cfe02160f036054f33ff7539c895f3fb9
uncleVich <kompleev@gmail.com>
2021-11-17 00:04:44 +0300

    задание 3

---
+++ src/main/java/com/sbrf/reboot/service/AccountService.java
@@ -0,0 +1,18 @@
+package com.sbrf.reboot.service;
+
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class AccountService {
+
+    @NonNull
+    AccountRepository accountRepository;
+
+    public boolean isAccountExist(long id, Account account) {
+        return accountRepository.getAllAccountsByClientId(id).contains(account);
+    }
+}
